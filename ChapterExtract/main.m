7e3e9a8583eb6c6f4470ce10594a7cefca88b21e
Max <mczonk@gmail.com>
2014-01-20 20:42:23 +0100

    Initial Commit

---
+++ ChapterExtract/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ChapterExtract
+//
+//  Created by Maximilian Christ on 20/01/14.
+//  Copyright (c) 2014 McZonk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
